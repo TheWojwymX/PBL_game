816120b7183e005d5883b7ca1db57bad7973013c
Ununheks <ununheks@gmail.com>
2024-10-22 13:36:05 +0200

    Remove turretmanager

--- src/main.cpp
+++ src/main.cpp
@@ -207,11 +207,7 @@ int main(int, char**)
     auto particleShader = RESOURCEMANAGER.GetShaderByName("particleShader");
     auto rangeShader = RESOURCEMANAGER.GetShaderByName("turretRangeShader");
     auto shovelShader = RESOURCEMANAGER.GetShaderByName("shovelShader");
-    auto blueprintShader = RESOURCEMANAGER.GetShaderByName("blueprintShader");
     auto glowstickShader = RESOURCEMANAGER.GetShaderByName("glowstickShader");
-
-
-    TURRETSMANAGER.Init();
 
     TUTORIALMANAGER.Init();
 
@@ -254,7 +250,6 @@ int main(int, char**)
 
             if (!GAMEMANAGER._paused) {
                 ENEMIESMANAGER.Update();
-                TURRETSMANAGER.Update();
                 UPGRADEMANAGER.Update();
                 DOMEMANAGER.Update();
                 GAMEMANAGER.Update();
@@ -382,14 +377,6 @@ int main(int, char**)
             rangeShader->use();
             rangeShader->setMat4("projection", projection);
             rangeShader->setMat4("view", view);
-
-            ///
-            blueprintShader->use();
-
-            blueprintShader->setVec3("viewPos", viewPos);
-            blueprintShader->setMat4("projection", projection);
-            blueprintShader->setMat4("view", view);
-            blueprintShader->setVec3("additionalColor", TURRETSMANAGER._additionalColor);
 
             GAMEMANAGER.root->Render(Transform::Origin());
             TRANSPARENT_RENDERER.Render();
